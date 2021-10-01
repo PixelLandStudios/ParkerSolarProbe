345fc55902f3729439a04d2ede70b0cdb5c76023
PixelLand Studios <mohammad.alown@gmail.com>
2021-10-01 15:19:41 +0300

    Added collider to PSP prefab

--- ParkerSolarProbe/Assets/Scripts/GazeScript.cs
+++ ParkerSolarProbe/Assets/Scripts/GazeScript.cs
@@ -11,14 +11,15 @@ public class GazeScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("1111111111");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, 3f))
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, 6f))
         {
+            Debug.Log(hit.collider.gameObject.name);
             if (hit.collider.gameObject.tag == "Probe")
             {
                 selectedProbe = hit.collider.gameObject;
